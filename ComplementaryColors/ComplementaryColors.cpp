6ae954672af80b6d7cfaccf61f76386330f14c16
Claus Lensbøl <claus@fab-it.dk>
2013-08-05 12:46:59 +0200

    Edit: Multiply the return color by 255 to get the right notation

--- ComplementaryColors/ComplementaryColors.cpp
+++ ComplementaryColors/ComplementaryColors.cpp
@@ -113,6 +113,11 @@ void _hsv2rgb(double color[]) {
     if (i == 3) { color[0] = m; color[1] = n; color[2] = v; }
     if (i == 4) { color[0] = k; color[1] = m; color[2] = v; }
     if (i == 5) { color[0] = v; color[1] = m; color[2] = n; }
+
+    // Multiply the colors by 255 to get the RGB representation
+    color[0] = color[0] * 255;
+    color[1] = color[1] * 255;
+    color[2] = color[2] * 255;
   }
 }
 
